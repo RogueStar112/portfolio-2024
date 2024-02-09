922473baacb14d6c7ccce28e6d8ceeb8f69d792a
RogueStar112 <hildtgunnt@gmail.com>
2024-02-09 16:23:22 +0000

    Portfolio nearing completion

--- src/app/components/ImageCollection.tsx
+++ src/app/components/ImageCollection.tsx
@@ -51,7 +51,7 @@ export default function ImageCollection({images}: ImageCollectionType) {
               )
             } else {
               return (
-                <div key={`img_div_${index}`} className="relative /md:[&>span]:hidden [&>span]:hover:block [&>span]:hover:absolute [&>span]:hover:bottom-0 [&>span]:bg-onyx [&>span]:p-2 [&>span]:text-white [&>span]:duration-150 col-span-4 order-first">
+                <div key={`img_div_${index}`} className="relative /md:[&>span]:hidden [&>span]:hover:block [&>span]:hover:absolute [&>span]:hover:bottom-0 [&>span]:bg-onyx dark:[&>span]:bg-black [&>span]:p-2 [&>span]:text-white [&>span]:duration-150 col-span-4 order-first">
                   <Image key={`img_${index}`} className="col-span-4 order-first rounded-t-lg overflow-mask-vert h-[400px] object-cover is-main-image" src={image.file_path} width={864} height={400} alt={"Test"}/>
                   <span key={`img_span_${index}`} className={`absolute bottom-0 w-full md:w-fit select-none ${image.caption ? "" : "hidden"}`}>{image.caption ? image.caption : ""}</span>
                 </div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -215,12 +215,12 @@ export default async function Home() {
 
 
 
-            <article className='collapse md:visible grid grid-cols-3 auto-rows-[192px] gap-4'>
+            <article className='/collapse md:visible grid grid-cols-3 auto-rows-[81px] md:auto-rows-[192px] gap-4'>
               {[...Array(7)].map((_, i) => {
                     return (
                         <div key={`bento-${i}`} className={`rounded-xl border-2 border-green-400/10 dark:bg-onyx bg-neutral-100 place-items-center ${i === 0 ? "col-span-2 row-span-2 primary-project"  : ""} ${i === 4 || i === 5 ? "row-span-2 secondary-project" : ""} ${i === 3 || i === 6 || i === 2 || i === 1 ? "tertiary-project" : ""}`}>
                               
-                              <h2 className={`${i === 0 ? "col-span-2 row-span-2 text-4xl" : "text-lg"}`}>
+                              <h2 className={`${i === 0 ? "md:col-span-2 md:row-span-2 text-4xl [&>*]:block [&>*]:overflow-auto" : "text-lg [&>*]:block [&>*]:overflow-auto"}`}>
                                   {/* {i} */}
                                   {/* {i === 0 ? "MyBudget" : ""}
                                   {i === 1 ? "Drag n' Drop Pizza Game" : ""}
@@ -228,8 +228,8 @@ export default async function Home() {
                                   {i === 3 ? "MoodCanvas" : ""} */}
 
                                   <Link href={`${i === 0 ? "https://github.com/RogueStar112/MyBudget" : ""}${i === 1 ? "https://pizza-game-oct2023.vercel.app/" : ""}${i === 3 ? "https://roguestar112.github.io/mood-journal/index.html" : ""}${i === 4 ? "https://github.com/RogueStar112/MyNutrition" : ""}${i === 5 ? "https://todoproject-dev.vercel.app/" : ""}`}>
-                                    <Image className='rounded-lg'  src={`${i === 0 ? "/images/MyBudgetBannerv4.png" : ""}${i === 1 ? "/images/PizzaGamev2.png" : ""}${i === 3 ? "/images/MoodCanvasBannerv2.png" : ""}${i === 4 ? "/images/MyNutritionProjectGridv3.png" : ""}${i === 5 ? "/images/TodoAppPlaceholder.png" : ""}${i === 6 ? "/images/RouteRaterTBA.png" : ""}`} 
-width={`${i === 0 ? "570" : "0"}${i === 4 || i === 5 || i == 6 || i == 3|| i == 1 ? "277" : "0"}`} height={`${i === 0|| i == 1 || i === 4 || i === 5 ? "400" : "0"}${i === 3 || i === 6  ? "192" : ""}`} alt={'BentoImage'}/>
+                                    <Image className='rounded-lg'  src={`${i === 0 ? "/images/MyBudgetBannerv4.png" : ""}${i === 1 ? "/images/PizzaGamev2.png" : ""}${i === 2 ? "/images/MyJournalTBA.png" : ""}${i === 3 ? "/images/MoodCanvasBannerv2.png" : ""}${i === 4 ? "/images/MyNutritionProjectGridv3.png" : ""}${i === 5 ? "/images/TodoAppPlaceholder.png" : ""}${i === 6 ? "/images/RouteRaterTBA.png" : ""}`} 
+width={`${i === 0 ? "570" : "0"}${i === 4 || i === 5 || i == 2 || i == 6 || i == 3|| i == 1 ? "277" : "0"}`} height={`${i === 0|| i == 1 || i == 2 || i === 4 || i === 5 ? "400" : "0"}${i === 3 || i === 6  ? "192" : ""}`} alt={'BentoImage'}/>
                                   </Link>
                                   {/* {i === 5 ? "Todo App" : ""}
                                   {i === 6 ? "Route Rater" : ""} */}
@@ -237,11 +237,11 @@ width={`${i === 0 ? "570" : "0"}${i === 4 || i === 5 || i == 6 || i == 3|| i == 
                                   {i === 5 ? "Todo App" : ""} */}
                               </h2>
 
-                              <div aria-label="project-tags" className={`${i === 4 || i === 5 ? "w-full h-full" : ""}`}>
-                                {/* {i === 3 ? "Mood tracking app. Done as a group project." : ""}
+                              {/* <div className="collapse" aria-label="project-tags" className={`${i === 4 || i === 5 ? "w-full h-full" : ""}`}>
+                                {i === 3 ? "Mood tracking app. Done as a group project." : ""}
                                 {i === 4 ? "Nutrition-tracking app. Powered with PHP & Laravel." : ""}
-                                {i === 5 ? "Get stuff done and on time with this mobile-friendly app!" : ""} */}
-                              </div>
+                                {i === 5 ? "Get stuff done and on time with this mobile-friendly app!" : ""}
+                              </div> */}
 
                         </div>
                     )  
