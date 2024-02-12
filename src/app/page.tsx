82f5b579f8a358f460e7ea1e882dc5439f6db507
DMistica <hildtgunnt@gmail.com>
2024-02-12 13:54:28 +0000

    Reworked blog article styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -50,7 +50,7 @@ export default async function Home() {
 
         <header className='grid grid-cols-2 justify-between relative sticky top-0 bg-inherit z-50 py-2'>
           
-          <div className='flex flex-col flex-start [&>*]:leading-[1.1rem] grow'>
+          <div className='flex flex-col justify-center [&>*]:leading-[1.1rem] grow'>
 
             <Link className='flex flex-col justify-center [&>*]:leading-[1.1rem]' href={'/'}>
               <p className='text-2xl font-extrabold'>DEMIE</p> 
@@ -261,7 +261,7 @@ width={`${i === 0 ? "570" : "0"}${i === 4 || i === 5 || i == 2 || i == 6 || i ==
                   let isLastElement = (blog_JSON[blog_JSON.length - 1].title === blog.title);
                   // ${isLastElement ? "col-span-full" : ""}
                   return (
-                  <article key={blog.id} id={blog.id} className={`/border-2 /border-blue-500 /dark:border-orange-500  overflow-hidden rounded-lg /max-w-[100%] /md:max-w-[33.3%] flex flex-col justify-start content-end ${isLastElement ? "col-span-full" : ""}`}>
+                  <article key={blog.id} id={blog.id} className={`border-2 border-green-400/10 /dark:border-orange-500  overflow-hidden rounded-lg /max-w-[100%] /md:max-w-[33.3%] flex flex-col justify-start content-end ${isLastElement ? "col-span-full" : ""}`}>
                     
                     <Link href={`blog/${blog.id}`}>
                       <Image className="rounded-t-lg overflow-mask-vert overflow-hidden h-[250px] object-cover hover:scale-110 duration-150" src={blog.images[0].file_path} width={864} height={300} alt={"Test"}/>
@@ -272,24 +272,33 @@ width={`${i === 0 ? "570" : "0"}${i === 4 || i === 5 || i == 2 || i == 6 || i ==
                    
                       <Link href={`blog/${blog.id}`} className={`text-blue-500 dark:text-orange-500 font-extrabold text-left /border-b-4 dark:border-orange-500 hover:text-orange-500 duration-200 ${isLastElement ? "text-3xl" : "text-xl md:text-base"}`} style={{fontVariant: "small-caps"}}>{blog.title}</Link>
 
+
+                
                          {/* <p className='text-stone-500 text-left'>{blog.dateTime_created}</p> */}
 
 
                       <p className={`${isLastElement ? "text-center" : "text-justify"}`}>{blog.teaser_content}</p>
-                    </section>
 
-
-
-                             <section className='relative p-4 flex justify-center gap-1 flex-1 justify-self-end' aria-label='post-tags'>
+                      <section className='relative py-4 flex justify-center gap-1 flex-1 justify-self-end' aria-label='post-tags'>
+                              
                               {/* <span className='absolute text-2xl md:text-5xl left-0 md:right-0 z-10 user-select opacity-35 select-none top-0 text-blue-500 dark:text-orange-500'>#</span> */}
-                        {blog.tags.map((tag: any) => {
-                          
-                            return (
-                                <Link key={tag} href={`/blog/tag/${tag}`} className='z-0 bg-blue-400 grow dark:bg-orange-700 dark:hover:bg-blue-700 hover:bg-orange-700 duration-200 p-2 text-white text-sm max-h-[40px]'>{tag}</Link>
-                            )
+                                {blog.tags.map((tag: any) => {
+                                  
+                                    return (
+                                        <Link key={tag} href={`/blog/tag/${tag}`} className='z-0 bg-blue-400 grow dark:bg-orange-700 dark:hover:bg-blue-700 hover:bg-orange-700 duration-200 p-2 text-white text-sm max-h-[40px] rounded-full'>{tag}</Link>
+                                    )
 
-                        })}
+                                })}
+                            </section>
                     </section>
+                      
+                      {/* <section> */}
+                            <section className='w-full'>
+                              <Link className='block bg-blue-400 grow dark:bg-orange-700 dark:hover:bg-blue-700 hover:bg-orange-700 duration-200 p-4 w-full text-white rounded-b-lg' key={`read-more-${blog.id}`} href={`/blog/${blog.id}`}>Read more</Link>
+                            </section>
+
+                            
+                    {/* </section> */}
                   </article>
 
 
