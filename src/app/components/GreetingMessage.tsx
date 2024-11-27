a4c4afd1c86fd0c92588ad5635a5f9b9b5daafe0
RogueStar112 <97138925+RogueStar112@users.noreply.github.com>
2024-11-27 06:10:19 +0000

    Animation test

--- src/app/components/GreetingMessage.tsx
+++ src/app/components/GreetingMessage.tsx
@@ -6,23 +6,24 @@ import { FaGithub } from "react-icons/fa"
 import { FaLinkedin } from "react-icons/fa"
 import { PiReadCvLogo } from "react-icons/pi"
 
-import { Inter, Teko, Montserrat, Merriweather, EB_Garamond } from 'next/font/google'
+import { EB_Garamond } from 'next/font/google'
 
-const montserrat = Montserrat({ weight: ['300', '500', '700', '800', '900'], style: ['normal', 'italic'], subsets: ['latin'] })
-const merriweather = Merriweather({ weight: ['300', '400', '700'], style: ['normal', 'italic'], subsets: ['latin'] })
-const teko = Teko({ weight: ['300', '400', '700'], style: ['normal'], subsets: ['latin'] })
+import { motion } from "motion/react"
+import { AnimatePresence } from "motion/react"
+
 const eb_garamond = EB_Garamond({weight: ['500', '600'], style: ['normal'], subsets: ['latin']});
 
 
 export default function GreetingMessage() {
   return (
     <section className='md:absolute bottom-0 left-4 md:right-0 mr-4 h-screen sm:h-full text-black md:text-white dark:text-white flex flex-col items-center md:items-end justify-center /md:justify-end /mt-12'>
-                      <p className='text-center md:text-right text-3xl md:text-4xl font-extrabold'>GREETINGS.</p>
-                      <p className={`${eb_garamond.className} font-initial md:text-right`}>Web Developer | Lifestyle Enthusiast<br></br>📍 Cambridge, UK</p>
+                      <motion.p className='text-center md:text-right text-3xl md:text-4xl font-extrabold' initial={{y: -50, opacity: 0}} animate={{ y: 0, opacity: 1, transition: { duration: 0.6, ease: "easeInOut" }  }}>GREETINGS.</motion.p>
+                      <motion.p className={`${eb_garamond.className} font-initial md:text-right`} initial={{opacity: 0}} animate={{ opacity: 1, transition: { duration: 0.6, ease: "easeInOut" }  }}>Web Developer | Lifestyle Enthusiast</motion.p>
+                      <motion.p className={`${eb_garamond.className} font-initial md:text-right`} initial={{opacity: 0}} animate={{ opacity: 1, transition: { duration: 0.6, ease: "easeInOut" }  }}>📍 Cambridge, UK</motion.p>
 
 
                       <div className='flex-col w-full'>
-                      <p className={`${eb_garamond.className} font-initial md:text-right`}>____________________</p>
+                      <motion.p className={`${eb_garamond.className} font-initial md:text-right`} initial={{x: 250, opacity: 0}} animate={{x: 0, opacity: 1, transition: { duration: 1.2, ease: "easeInOut" }  }}>____________________</motion.p>
                       <section id='socials' className='flex justify-around md:justify-end m-4'>
 
                                 <div className='flex gap-8 [&>*]:[&>*]:scale-[2] /[&>*]:[&>*]:text-neutral-500' aria-label='socials'>
