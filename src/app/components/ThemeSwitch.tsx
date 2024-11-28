f006f7935b5a86e33a991de04f4dd8e5190cefea
DMistica <hildtgunnt@gmail.com>
2024-11-28 01:11:11 +0000

    Analytics Tracking events: Greetings and Themes

--- src/app/components/ThemeSwitch.tsx
+++ src/app/components/ThemeSwitch.tsx
@@ -6,6 +6,8 @@ import { useState, useEffect } from 'react'
 import { useTheme } from '../../../node_modules/next-themes/dist/index'
 import Image from 'next/image'
 
+import { track } from '@vercel/analytics';
+
 export default function ThemeSwitch() {
   const [mounted, setMounted] = useState(false)
   const { setTheme, resolvedTheme } = useTheme()
@@ -30,20 +32,20 @@ export default function ThemeSwitch() {
   }
 
   if (resolvedTheme === 'dark') {
-    return <button style={{fontVariant: "small-caps"}} className="flex gap-4 dark:bg-white dark:text-onyx w-[40px] rounded-full p-2" onClick={() => setTheme('light')}><FiSun className="[&>*]:p-2 m-auto"></FiSun><span className="hidden md:visible">Light Mode</span></button>
+    return <button style={{fontVariant: "small-caps"}} className="flex gap-4 dark:bg-white dark:text-onyx w-[40px] rounded-full p-2" onClick={() => {setTheme('light'); track('Light Mode Set')}}><FiSun className="[&>*]:p-2 m-auto"></FiSun><span className="hidden md:visible">Light Mode</span></button>
   }
 
   if (resolvedTheme === 'dark-dys') {
-    return <button style={{fontVariant: "small-caps"}} className="flex gap-4 bg-white text-onyx w-[40px] rounded-full p-2" onClick={() => {setTheme('light-dys'); toggleDarkDysToBody();}}><FiSun className="[&>*]:p-2 m-auto"></FiSun><span className="hidden md:visible">Light Mode</span></button>
+    return <button style={{fontVariant: "small-caps"}} className="flex gap-4 bg-white text-onyx w-[40px] rounded-full p-2" onClick={() => {setTheme('light-dys'); toggleDarkDysToBody(); track('Light Dys Set')}}><FiSun className="[&>*]:p-2 m-auto"></FiSun><span className="hidden md:visible">Light Mode</span></button>
   }
 
 
   if (resolvedTheme === 'light-dys') {
-     return <button style={{fontVariant: "small-caps"}} className="flex gap-4 bg-onyx text-white w-[40px] rounded-full p-2" onClick={() => {setTheme('dark-dys'); toggleDarkDysToBody();}}><FiMoon className="[&>*]:p-2 m-auto"></FiMoon><span className="hidden md:visible">Light Mode</span></button>
+     return <button style={{fontVariant: "small-caps"}} className="flex gap-4 bg-onyx text-white w-[40px] rounded-full p-2" onClick={() => {setTheme('dark-dys'); toggleDarkDysToBody();  track('Dark Dys Set')}}><FiMoon className="[&>*]:p-2 m-auto"></FiMoon><span className="hidden md:visible">Light Mode</span></button>
   }
 
   if (resolvedTheme === 'light') {
-    return <button style={{fontVariant: "small-caps"}} className="flex gap-4 bg-onyx text-white w-[40px] rounded-full p-2" onClick={() => setTheme('dark')}>
+    return <button style={{fontVariant: "small-caps"}} className="flex gap-4 bg-onyx text-white w-[40px] rounded-full p-2" onClick={() => {setTheme('dark'); track('Dark Mode Set')}}>
 <FiMoon className="dark:bg-onyx rounded-full [&>*]:p-2 m-auto"></FiMoon><span className="hidden md:visible">Dark Mode</span></button>
   }
 
